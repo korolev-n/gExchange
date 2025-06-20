6310c0df91867e22c4b1aba7bd088a35d7116f97
Nikolai Korolev <korolev.n@hotmail.com>
2025-06-20 16:13:42 +0300

    feat: добавить gRPC-сервис

---
+++ exchanger/internal/transport/grpc/handler.go
@@ -0,0 +1,32 @@
+package grpc
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/korolev-n/gExchange/exchanger/api"
+	"github.com/korolev-n/gExchange/exchanger/internal/service"
+)
+
+type Handler struct {
+	api.UnimplementedExchangerServiceServer
+	Service *service.ExchangeService
+	Logger  *slog.Logger
+}
+
+func New(service *service.ExchangeService, logger *slog.Logger) *Handler {
+	return &Handler{
+		Service: service,
+		Logger:  logger,
+	}
+}
+
+func (h *Handler) GetRates(ctx context.Context, _ *api.Empty) (*api.RatesResponse, error) {
+	rates, err := h.Service.GetRates(ctx)
+	if err != nil {
+		h.Logger.Error("failed to get rates", "error", err)
+		return nil, err
+	}
+
+	return &api.RatesResponse{Rates: rates}, nil
+}
